59298f127f0e26a56b82df220d6f3cab7e757231
ROYGBIV <royston.phetmyxay24@student.cart.org>
2025-01-28 15:50:16 -0800

    finish this up b4 friday stupiud

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ const ctx = canvas.getContext("2d")
 
 // previous mouse positions; will be null initially
 
+let prevX = null
+let prevY = null
+
+// line thickness
+ctx.lineWidth = 5
+
 // tracking mouse position
 window.addEventListener("mousemove", (e) =>) {
-    console.log("Mouse X: " + e.clientX)
-    console.log("Mouse Y: " + e.clientY)
+    // can't draw line bc position starts as null
+    if(prevX == null || prevY == null){
+        //set previous mouse position to current
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
 }
